127178f65afd4a1f24eea46181b81758fee4eb06
Kevin <kevindavidoppenheimer@gmail.com>
2025-11-23 21:53:23 -0300

    ej3 empezado

--- P1-ComDavid-2c2025/src/ej2/ej2.c
+++ P1-ComDavid-2c2025/src/ej2/ej2.c
@@ -1,5 +1,13 @@
 #include "../ejs.h"
 
-catalogo_t *removerCopias(catalogo_t *h) {
-   return (catalogo_t*)NULL;
+
+catalogo_t *removerCopias(catalogo_t *h)
+{
+   publicacion_t *actual = h->first;
+   while (actual != NULL && actual->next != NULL)
+   {
+      
+      actual = actual->next;
+   }
+   return h;
 }
