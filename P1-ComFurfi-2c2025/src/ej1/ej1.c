7d2df0a182a547d33258047fc157a74ebc880465
Kevin <kevindavidoppenheimer@gmail.com>
2025-11-26 03:13:45 -0300

    ej1 en C

--- P1-ComFurfi-2c2025/src/ej1/ej1.c
+++ P1-ComFurfi-2c2025/src/ej1/ej1.c
@@ -1,9 +1,27 @@
 #include "../ejs.h"
 #include <string.h>
 
-// Función principal: publicar un tuit
-tuit_t *publicar(char *mensaje, usuario_t *user) {
+void agregarAFeed(tuit_t *nuevo_tuit, feed_t *feed)
+{
+  publicacion_t *nueva_publicacion = malloc(sizeof(publicacion_t));
+  nueva_publicacion->value = nuevo_tuit;
+  nueva_publicacion->next = feed->first;
+  feed->first = nueva_publicacion;
+}
 
-  return NULL;
-  
+// Función principal: publicar un tuit
+tuit_t *publicar(char *mensaje, usuario_t *user)
+{
+  tuit_t *nuevo_tuit = calloc(1, sizeof(tuit_t));
+  strcpy(nuevo_tuit->mensaje, mensaje);
+  nuevo_tuit->id_autor = user->id;
+  agregarAFeed(nuevo_tuit, user->feed);
+  usuario_t *actual;
+  for (uint16_t i = 0; i < user->cantSeguidores; i++)
+  {
+    if (user->seguidores[i]->feed == NULL)
+      continue;
+    agregarAFeed(nuevo_tuit, user->seguidores[i]->feed);
+  }
+  return nuevo_tuit;
 }
