b52d79cbe1d07986bce5e3c599db684413817cb3
Kevin <kevindavidoppenheimer@gmail.com>
2025-11-27 05:48:34 +0000

    ej2 terminado con un poco de ayuda

--- P1-ComFurfi-2c2025/src/ej2/ej2.asm
+++ P1-ComFurfi-2c2025/src/ej2/ej2.asm
@@ -8,29 +8,105 @@ section .text
 
 ; Completar las definiciones (serán revisadas por ABI enforcer):
 TUIT_MENSAJE_OFFSET EQU 0
-TUIT_FAVORITOS_OFFSET EQU 0
-TUIT_RETUITS_OFFSET EQU 0
-TUIT_ID_AUTOR_OFFSET EQU 0
-TUIT_SIZE EQU 0
+TUIT_FAVORITOS_OFFSET EQU 140
+TUIT_RETUITS_OFFSET EQU 142
+TUIT_ID_AUTOR_OFFSET EQU 144
+TUIT_SIZE EQU 148
 
 PUBLICACION_NEXT_OFFSET EQU 0
-PUBLICACION_VALUE_OFFSET EQU 0
-PUBLICACION_SIZE EQU 0
+PUBLICACION_VALUE_OFFSET EQU 8
+PUBLICACION_SIZE EQU 16
 
 FEED_FIRST_OFFSET EQU 0 
-FEED_SIZE EQU 0
+FEED_SIZE EQU 8
 
 USUARIO_FEED_OFFSET EQU 0;
-USUARIO_SEGUIDORES_OFFSET EQU 0; 
-USUARIO_CANT_SEGUIDORES_OFFSET EQU 0; 
-USUARIO_SEGUIDOS_OFFSET EQU 0; 
-USUARIO_CANT_SEGUIDOS_OFFSET EQU 0; 
-USUARIO_BLOQUEADOS_OFFSET EQU 0; 
-USUARIO_CANT_BLOQUEADOS_OFFSET EQU 0; 
-USUARIO_ID_OFFSET EQU 0; 
-USUARIO_SIZE EQU 0
+USUARIO_SEGUIDORES_OFFSET EQU 8; 
+USUARIO_CANT_SEGUIDORES_OFFSET EQU 16; 
+USUARIO_SEGUIDOS_OFFSET EQU 24; 
+USUARIO_CANT_SEGUIDOS_OFFSET EQU 32; 
+USUARIO_BLOQUEADOS_OFFSET EQU 40; 
+USUARIO_CANT_BLOQUEADOS_OFFSET EQU 48; 
+USUARIO_ID_OFFSET EQU 52; 
+USUARIO_SIZE EQU 56
 
 ; void bloquearUsuario(usuario_t *usuario, usuario_t *usuarioABloquear);
 global bloquearUsuario 
 bloquearUsuario:
-ret
+    push rbp
+    mov rbp, rsp
+    
+    push rbx
+    push r12
+
+    mov rbx, rdi
+    mov r12, rsi
+
+    mov edi, [rbx + USUARIO_CANT_BLOQUEADOS_OFFSET]
+    mov rsi, [rbx + USUARIO_BLOQUEADOS_OFFSET]
+
+    mov [rsi + rdi*8], r12
+
+    inc DWORD [rbx + USUARIO_CANT_BLOQUEADOS_OFFSET]
+
+    mov rdi, [rbx + USUARIO_FEED_OFFSET]
+    mov rsi, r12
+    call borrarPublicacionesDe
+
+    mov rdi, [r12 + USUARIO_FEED_OFFSET]
+    mov rsi, rbx
+    call borrarPublicacionesDe
+
+    .epilogo:
+        pop r12
+        pop rbx
+        pop rbp
+        ret
+
+borrarPublicacionesDe:
+    push rbp
+    mov rbp, rsp
+
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 8
+
+    ; rbx = indirect, r12 = usuario->id, r13 = actual
+    lea rbx, [rdi + FEED_FIRST_OFFSET]
+    mov r12D, [rsi + USUARIO_ID_OFFSET]
+
+    .loop:
+        ; r13 = actual
+        mov r13, [rbx]
+        test r13, r13
+        jz .epilogo
+
+        mov rsi, [r13 + PUBLICACION_VALUE_OFFSET]
+        mov esi, [rsi + TUIT_ID_AUTOR_OFFSET]
+        cmp esi, r12D
+        jne .else
+
+        mov rsi, [r13 + PUBLICACION_NEXT_OFFSET]
+        mov [rbx], rsi
+
+        mov rdi, r13
+        call free
+        jmp .loop
+
+    .else:
+        lea rbx, [r13 + PUBLICACION_NEXT_OFFSET]
+        jmp .loop
+
+    .epilogo:
+        add rsp, 8
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop rbx
+        pop rbp
+        ret
+
--- P1-ComFurfi-2c2025/src/ej2/ej2.c
+++ P1-ComFurfi-2c2025/src/ej2/ej2.c
@@ -2,17 +2,27 @@
 
 void borrarPublicacionesDe(feed_t *feed, usuario_t *usuario)
 {
-  publicacion_t *actual = feed->first;
-  publicacion_t a_borrar;
-  while (actual != NULL)
+  publicacion_t **indirect = &(feed->first);
+  publicacion_t *actual;
+  uint32_t id_user = usuario->id;
+  while (*indirect != NULL)
   {
+    actual = *indirect;
+    if (actual->value->id_autor == id_user)
+    {
+      *indirect = actual->next;
+      free(actual);
+    }
+    else
+      indirect = &(actual->next);
   }
 }
 
 void bloquearUsuario(usuario_t *usuario, usuario_t *usuarioABloquear)
 {
   usuario->bloqueados[usuario->cantBloqueados] = usuarioABloquear;
-  usuario->bloqueados += 1;
-
+  usuario->cantBloqueados++;
+  borrarPublicacionesDe(usuario->feed, usuarioABloquear);
+  borrarPublicacionesDe(usuarioABloquear->feed, usuario);
   return;
 }
