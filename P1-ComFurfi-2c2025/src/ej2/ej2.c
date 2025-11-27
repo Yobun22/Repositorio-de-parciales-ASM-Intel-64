b7f201b2d055b632cfeb3e8ef6c74e129b06a117
Kevin <kevindavidoppenheimer@gmail.com>
2025-11-27 00:34:32 -0300

    ej2 empezado

--- P1-ComFurfi-2c2025/src/ej2/ej2.c
+++ P1-ComFurfi-2c2025/src/ej2/ej2.c
@@ -1,8 +1,18 @@
 #include "../ejs.h"
 
-void bloquearUsuario(usuario_t *usuario, usuario_t *usuarioABloquear){
+void borrarPublicacionesDe(feed_t *feed, usuario_t *usuario)
+{
+  publicacion_t *actual = feed->first;
+  publicacion_t a_borrar;
+  while (actual != NULL)
+  {
+  }
+}
+
+void bloquearUsuario(usuario_t *usuario, usuario_t *usuarioABloquear)
+{
   usuario->bloqueados[usuario->cantBloqueados] = usuarioABloquear;
   usuario->bloqueados += 1;
-  
+
   return;
 }
